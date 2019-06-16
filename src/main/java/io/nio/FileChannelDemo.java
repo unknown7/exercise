4f4b72c5e7bbe18b4e5c1b5c116b89a9d55c426c
unknown7 <unknown7@live.cn>
2019-06-16 18:27:53 +0800

    nio

--- src/main/java/io/nio/FileChannelDemo.java
+++ src/main/java/io/nio/FileChannelDemo.java
@@ -1,4 +1,19 @@
 package io.nio;
 
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
 public class FileChannelDemo {
+
+    public static void main(String[] args) throws IOException {
+        RandomAccessFile rw = new RandomAccessFile("FileChannelDemo.java", "rw");
+        FileChannel channel = rw.getChannel();
+        ByteBuffer buffer = ByteBuffer.allocate(1024);
+        int read = channel.read(buffer);
+        while (read != -1) {
+
+        }
+    }
 }
