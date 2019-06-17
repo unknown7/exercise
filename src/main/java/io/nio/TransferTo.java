8fc83537d36ae0d728de40ff57825ba30fddee25
unknown7 <unknown7@live.cn>
2019-06-17 14:11:39 +0800

    nio

--- src/main/java/io/nio/TransferTo.java
+++ src/main/java/io/nio/TransferTo.java
@@ -3,15 +3,20 @@ package io.nio;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.RandomAccessFile;
 import java.nio.channels.FileChannel;
 
 public class TransferTo {
 	@SuppressWarnings("resource")
 	public static void main(String[] args) throws IOException {
-		FileChannel
-		in = new FileInputStream("data.txt").getChannel(),
-		out = new FileOutputStream("data2.txt").getChannel();
-		
-		in.transferTo(0, in.size(), out);
+//		FileChannel
+//		in = new FileInputStream("data.txt").getChannel(),
+//		out = new FileOutputStream("data2.txt").getChannel();
+//
+//		in.transferTo(0, in.size(), out);
+
+		FileChannel from = new RandomAccessFile("D:\\projects\\idea\\exercise\\src\\main\\java\\io\\nio\\TransferTo.java", "rw").getChannel();
+		FileChannel to = new RandomAccessFile("D:\\projects\\idea\\exercise\\src\\main\\java\\io\\nio\\TransferToTemp.java", "rw").getChannel();
+		from.transferTo(0, from.size(), to);
 	}
 }
