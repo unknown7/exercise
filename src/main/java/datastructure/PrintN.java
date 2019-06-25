fe50ef5c1553fcd1f962c7f22077fce1281d5832
unknown7 <unknown7@live.cn>
2019-06-25 23:03:51 +0800

    data structure

---
+++ src/main/java/datastructure/PrintN.java
@@ -0,0 +1,28 @@
+package datastructure;
+
+public class PrintN {
+    int n;
+    public PrintN(int n) {
+        this.n = n;
+    }
+    public static void main(String[] args) {
+        PrintN printN = new PrintN(7);
+        printN.loop();
+        System.err.println();
+        printN.recursion(1);
+    }
+    void loop() {
+        System.err.println("loop..");
+        for (int i = 1; i <= n; i++) {
+            System.err.print(i + " ");
+        }
+    }
+    void recursion(int t) {
+        if (t == 1)
+            System.err.println("recursion..");
+        if (t <= n) {
+            System.err.print(t + " ");
+            recursion(++t);
+        }
+    }
+}
