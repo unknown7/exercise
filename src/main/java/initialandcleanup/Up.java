facbebad48e88b36d0e0e779aac6fcad82283fe8
unknown7 <unknown7@live.cn>
2019-07-01 11:50:45 +0800

    initial and cleanup

--- src/main/java/initialandcleanup/Up.java
+++ src/main/java/initialandcleanup/Up.java
@@ -11,5 +11,25 @@ public class Up {
     public static void main(String[] args) {
         int x = 5;
         new Up(x);
+        new Up(x).new Inner();
+        new Up.StaticInner();
+    }
+
+    int x;
+    static int y;
+    class Inner {
+        Inner() {
+            System.err.println(x);
+            System.err.println(y);
+            System.err.println("initialize inner..");
+        }
+    }
+    static class StaticInner {
+        StaticInner() {
+//            不能访问非静态成员
+//            System.err.println(x);
+            System.err.println(y);
+            System.err.println("initialize static inner..");
+        }
     }
 }
