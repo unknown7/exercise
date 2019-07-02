02b972189e4906c1793a325322178fffdb850f42
unknown7 <unknown7@live.cn>
2019-07-02 22:37:25 +0800

    initial and cleanup

---
+++ src/main/java/initialandcleanup/Constructors.java
@@ -0,0 +1,22 @@
+package initialandcleanup;
+
+public class Constructors {
+    Constructors() {
+        System.err.println("Constructors..");
+    }
+    Constructors(int i) {
+        System.err.println("Constructor..(int i)");
+    }
+
+    public static void main(String[] args) {
+        Constructor constructor = new Constructor(1);
+    }
+}
+class Constructor extends Constructors {
+    Constructor() {
+        System.err.println("Constructor..");
+    }
+    Constructor(int i) {
+        System.err.println("Constructor..(int i)");
+    }
+}
