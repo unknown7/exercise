801524930e0272d63c363ed328da03419fb62066
unknown7 <unknown7@live.cn>
2019-07-05 20:52:14 +0800

    reuse

--- src/main/java/reuse/Interface.java
+++ src/main/java/reuse/Interface.java
@@ -4,15 +4,26 @@ public interface Interface {
     int MAX = 0;
     void p();
 }
-class InterfaceExt implements Interface {
-
+abstract class InterfaceExtAbs implements Interface {
     @Override
     public void p() {
         System.err.println(MAX);
+        System.err.println(this.toString());
     }
 
+    @Override
+    public String toString() {
+        return "InterfaceExtAbs";
+    }
+}
+class InterfaceExt extends InterfaceExtAbs {
     public static void main(String[] args) {
         Interface i = new InterfaceExt();
         i.p();
+    }
+
+    @Override
+    public String toString() {
+        return "InterfaceExt";
     }
 }
