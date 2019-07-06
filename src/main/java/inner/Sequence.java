3fb506e9c6d03598d755cc34e2e7bda504c5d4d7
unknown7 <unknown7@live.cn>
2019-07-06 23:31:19 +0800

    inner

---
+++ src/main/java/inner/Sequence.java
@@ -0,0 +1,53 @@
+package inner;
+
+interface Selector {
+    boolean end();
+    Object current();
+    void next();
+}
+public class Sequence {
+    private int[] items;
+    private int cur;
+    public Sequence(int size) {
+        items = new int[size];
+    }
+    public void add(int x) {
+        if (cur < items.length) {
+            items[cur++] = x;
+        }
+    }
+    private class SequenceSelector implements Selector {
+        private int i;
+
+        @Override
+        public boolean end() {
+            return i == items.length;
+        }
+
+        @Override
+        public Object current() {
+            return items[i];
+        }
+
+        @Override
+        public void next() {
+            i++;
+        }
+    }
+    public Selector selector() {
+        return new SequenceSelector();
+    }
+
+    public static void main(String[] args) {
+        final int SIZE = 10;
+        Sequence sequence = new Sequence(SIZE);
+        for (int i = 0; i < SIZE; i++) {
+            sequence.add(i);
+        }
+        Selector selector = sequence.selector();
+        while (!selector.end()) {
+            System.err.println(selector.current());
+            selector.next();
+        }
+    }
+}
