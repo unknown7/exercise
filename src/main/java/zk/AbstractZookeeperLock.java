f3cf49dbf781cdd194f8e71794d040c1b1c7aa67
unknown7 <unknown7@live.cn>
2019-07-12 11:48:46 +0800

    zk

---
+++ src/main/java/zk/AbstractZookeeperLock.java
@@ -0,0 +1,22 @@
+package zk;
+
+import org.I0Itec.zkclient.ZkClient;
+
+public abstract class AbstractZookeeperLock implements Lock {
+    private static final String CONNECT_URL = "127.0.0.1:2181";
+    protected ZkClient zkClient = new ZkClient(CONNECT_URL);
+    protected static final String PATH = "/lock";
+
+    public void lock() {
+        if (tryLock()) {
+            System.err.println("get lock..");
+        } else {
+            waitLock();
+            lock();
+        }
+    }
+
+    public abstract boolean tryLock();
+    public abstract boolean waitLock();
+    public abstract void unLock();
+}
