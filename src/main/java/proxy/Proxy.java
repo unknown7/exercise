2c1c0a2658d800f86e79d94d839b137b01f17f1e
unknown7 <unknown7@live.cn>
2019-07-12 16:16:43 +0800

    proxy

--- src/main/java/proxy/Proxy.java
+++ src/main/java/proxy/Proxy.java
@@ -4,8 +4,14 @@ public class Proxy {
     public static void main(String[] args) {
         User user = new User();
         user.setId(1);
-        user.setName("1");
-        UserService userService = new UserServiceImpl();
+        user.setName("wilford");
+        UserService us = new UserServiceImpl();
+        UserServiceInterceptor usi = new UserServiceInterceptor(us);
+        UserService userService = (UserService) java.lang.reflect.Proxy.newProxyInstance(
+                us.getClass().getClassLoader(),
+                us.getClass().getInterfaces(),
+                usi
+        );
         userService.save(user);
     }
 }
---
+++ src/main/java/proxy/UserServiceInterceptor.java
@@ -0,0 +1,22 @@
+package proxy;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+
+public class UserServiceInterceptor implements InvocationHandler {
+    private Object object;
+    public UserServiceInterceptor(Object object) {
+        this.object = object;
+    }
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        if (args.length > 0 && args[0] instanceof User) {
+            User user = (User) args[0];
+            if (user.getName().trim().length() <= 1)
+                throw new RuntimeException("The variable [name] was too short");
+        }
+        Object invoke = method.invoke(object, args);
+        System.err.println("invoke success..");
+        return invoke;
+    }
+}
