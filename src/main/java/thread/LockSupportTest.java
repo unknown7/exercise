110f6b1371b410e701ad70393a86a034be0debff
unknown7 <unknown7@live.cn>
2019-08-05 01:07:16 +0800

    minor revision

--- src/main/java/thread/LockSupportTest.java
+++ src/main/java/thread/LockSupportTest.java
@@ -1,14 +1,30 @@
 package thread;
 
-import java.util.concurrent.locks.LockSupport;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class LockSupportTest {
-    public static void main(String[] args) {
-        String s1 = null;
-        String s2 = null;
-        System.err.println(s1 == s2);
-        Thread.interrupted();
-        LockSupport.park();
-        System.err.println("block.");
+    private static ExecutorService exec = Executors.newCachedThreadPool();
+    public static void main(String[] args) throws InterruptedException {
+//        String s1 = null;
+//        String s2 = null;
+//        System.err.println(s1 == s2);
+//        Thread.interrupted();
+//        LockSupport.park();
+//        System.err.println("block.");
+
+        Lock lock = new ReentrantLock();
+        lock.lock();
+        exec.execute(new Runnable() {
+            @Override
+            public void run() {
+                lock.lock();
+            }
+        });
+        TimeUnit.SECONDS.sleep(3);
+        lock.unlock();
     }
 }
