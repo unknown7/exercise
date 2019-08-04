358a974c5757a901fec5b150173a53be87779134
unknown7 <unknown7@live.cn>
2019-08-04 12:02:53 +0800

    minor revision

--- src/main/java/thread/LockSupportTest.java
+++ src/main/java/thread/LockSupportTest.java
@@ -4,6 +4,9 @@ import java.util.concurrent.locks.LockSupport;
 
 public class LockSupportTest {
     public static void main(String[] args) {
+        String s1 = null;
+        String s2 = null;
+        System.err.println(s1 == s2);
         LockSupport.park();
         System.err.println("block.");
     }
