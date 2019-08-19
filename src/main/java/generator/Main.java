4abce7bd8e51855089fb82a5228a420519a3aad8
unknown7 <unknown7@live.cn>
2019-08-19 17:07:49 +0800

    minor revision

--- src/main/java/generator/Main.java
+++ src/main/java/generator/Main.java
@@ -7,7 +7,7 @@ import java.util.TreeSet;
 public class Main {
 	public static void main(String[] args) {
 		String[] strs = Generated.array(new RandomGenerator.String(), String.class, 7);
-		Set<String> set = new TreeSet<>(String.CASE_INSENSITIVE_ORDER);
+		Set<String> set = new TreeSet<String>(String.CASE_INSENSITIVE_ORDER);
 		set.addAll(Arrays.asList(strs));
 		System.err.println(set);
 	}
