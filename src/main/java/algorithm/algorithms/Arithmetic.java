863fcbeb79b1838ab1ad25af25a51b1342723d53
unknown7 <unknown7@live.cn>
2019-12-16 19:18:32 +0800

    stack

---
+++ src/main/java/algorithm/algorithms/Arithmetic.java
@@ -0,0 +1,74 @@
+package algorithm.algorithms;
+
+public class Arithmetic {
+
+    private enum Operator {
+        PLUS("+"), MINUS("-"), MULTIPLY("*"), DIVIDE("/"), LEFT_INTERVAL("("), RIGHT_INTERVAL(")"), DEFAULT("default");
+
+        private String operator;
+
+        Operator(String operator) {
+            this.operator = operator;
+        }
+
+        public static Operator get(String operator) {
+            for (Operator value : values()) {
+                if (value.operator.equals(operator)) {
+                    return value;
+                }
+            }
+            return DEFAULT;
+        }
+    }
+
+    private static final String EXPRESSION = "(((1+2)+(3+4))*(5*6))";
+
+    public static void main(String[] args) {
+        Stack<Operator> operators = new Stack<Operator>();
+        Stack<Integer> numbers = new Stack<Integer>();
+
+        for (int i = 0; i < EXPRESSION.length(); i++) {
+            String value = String.valueOf(EXPRESSION.charAt(i));
+            switch (Operator.get(value)) {
+                case PLUS:
+                    operators.push(Operator.PLUS);
+                    break;
+                case MINUS:
+                    operators.push(Operator.MINUS);
+                    break;
+                case MULTIPLY:
+                    operators.push(Operator.MULTIPLY);
+                    break;
+                case DIVIDE:
+                    operators.push(Operator.DIVIDE);
+                    break;
+                case LEFT_INTERVAL:
+                    break;
+                case RIGHT_INTERVAL:
+                    Operator operator = operators.pop();
+                    Integer sum = 0;
+                    Integer num = numbers.pop();
+                    switch (operator) {
+                        case PLUS:
+                            sum = numbers.pop() + num;
+                            break;
+                        case MINUS:
+                            sum = numbers.pop() - num;
+                            break;
+                        case MULTIPLY:
+                            sum = numbers.pop() * num;
+                            break;
+                        case DIVIDE:
+                            sum = numbers.pop() / num;
+                            break;
+                    }
+                    numbers.push(sum);
+                    break;
+                default:
+                    numbers.push(Integer.valueOf(value));
+                    break;
+            }
+        }
+        System.err.println(numbers.pop());
+    }
+}
---
+++ src/main/java/algorithm/algorithms/Stack.java
@@ -0,0 +1,35 @@
+package algorithm.algorithms;
+
+import java.util.Arrays;
+
+public class Stack<Item> {
+    private Item[] arr = (Item[]) new Object[10];
+    private int size = 0;
+
+    public void push(Item item) {
+        ensureCapacity();
+        arr[size++] = item;
+    }
+
+    public Item pop() {
+        if (!isEmpty())
+            return arr[--size];
+        return null;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    private void ensureCapacity() {
+        if (size + 1 >= arr.length) {
+            Item[] newArr = Arrays.copyOf(arr, size << 1);
+            arr = newArr;
+        }
+    }
+
+}
