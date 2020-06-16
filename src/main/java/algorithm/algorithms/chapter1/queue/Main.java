ee2cdfc070b18a15e6ded7ae6caed1790b9cdeb2
unknown7 <unknown7@live.cn>
2020-06-16 10:33:08 +0800

    stack contest

--- src/main/java/algorithm/algorithms/chapter1/queue/Main.java
+++ src/main/java/algorithm/algorithms/chapter1/queue/Main.java
@@ -3,7 +3,7 @@ package algorithm.algorithms.chapter1.queue;
 public class Main {
 
     public static void main(String[] args) {
-        Queue<Integer> queue = new ArrayQueue<>();
+        Queue<Integer> queue = new LinkedQueue<>();
         queue.enqueue(1);
         queue.enqueue(6);
         queue.enqueue(8);
---
+++ src/main/java/algorithm/algorithms/chapter1/queue/Parenthesis.java
@@ -0,0 +1,71 @@
+package algorithm.algorithms.chapter1.queue;
+
+import algorithm.algorithms.chapter1.stack.LinkedStack;
+import algorithm.algorithms.chapter1.stack.Stack;
+import org.springframework.util.StringUtils;
+
+import java.util.Arrays;
+
+public class Parenthesis {
+
+    private final static String EMPTY = "";
+
+    private final static String[] C = {"()", "[]", "{}"};
+
+    private final static Couple[] COUPLES = new Couple[C.length];
+
+    static {
+        for (int i = 0; i < C.length; i++) {
+            COUPLES[i] = new Couple(C[i]);
+        }
+    }
+
+    private boolean isPalindromic(String str) {
+        Stack<String> stack = new LinkedStack<>();
+        String[] split = str.split(EMPTY);
+        for (String s : split) {
+            if (Arrays.stream(COUPLES).anyMatch(item -> item.getSide(s) == Couple.Side.LEFT)) {
+                stack.push(s);
+            } else {
+                String pop = stack.pop();
+                if (Arrays.stream(COUPLES).noneMatch(item -> item.isCouple(pop, s))){
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        String str = "{()}(){[()]}";
+        Parenthesis p = new Parenthesis();
+        boolean palindromic = p.isPalindromic(str);
+        System.err.println(palindromic);
+    }
+
+    private static class Couple {
+
+        public enum Side {
+            LEFT, RIGHT
+        }
+
+        private String left;
+
+        private String right;
+
+        Couple(String str) {
+            assert !StringUtils.isEmpty(str);
+            String[] split = str.split(EMPTY);
+            left = split[0];
+            right = split[1];
+        }
+
+        boolean isCouple(String left, String right) {
+            return this.left.equals(left) && this.right.equals(right);
+        }
+
+        Side getSide(String c) {
+            return left.equals(c) ? Side.LEFT : Side.RIGHT;
+        }
+    }
+}
