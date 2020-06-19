3d753d1eb8aa40439ebefb91fce464dae87c89ae
unknown7 <unknown7@live.cn>
2020-06-19 17:39:32 +0800

    shell

---
+++ src/main/java/algorithm/algorithms/chapter2/Shell.java
@@ -0,0 +1,31 @@
+package algorithm.algorithms.chapter2;
+
+import java.util.Arrays;
+
+public class Shell {
+
+	public static void main(String[] args) {
+		int[] a = {7, 3, 1, 9 ,2, 19, 22, 24, 21, 11, 283, 123};
+		sort(a);
+		System.err.println(Arrays.toString(a));
+	}
+
+	static void sort(int[] a) {
+		int n = a.length;
+		int h = 1;
+
+		// 4, 13, 40, 121, 364, 1093...
+		while (h < n / 3) {
+			h = 3 * h + 1;
+		}
+
+		while (h >= 1) {
+			for (int i = h; i < n; i++) {
+				for (int j = i; j >= h && a[j] < a[j - h]; j-=h) {
+					Exchanger.exchange(a, j, j - h);
+				}
+			}
+			h /= 3;
+		}
+	}
+}
