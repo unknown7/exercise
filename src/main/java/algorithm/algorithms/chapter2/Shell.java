498a8118d050aa15353973db92b306d356cfe301
unknown7 <unknown7@live.cn>
2020-06-19 17:57:17 +0800

    shell

--- src/main/java/algorithm/algorithms/chapter2/Shell.java
+++ src/main/java/algorithm/algorithms/chapter2/Shell.java
@@ -6,7 +6,7 @@ public class Shell {
 
 	public static void main(String[] args) {
 		int[] a = {7, 3, 1, 9 ,2, 19, 22, 24, 21, 11, 283, 123};
-		sort(a);
+		shell(a);
 		System.err.println(Arrays.toString(a));
 	}
 
@@ -22,7 +22,25 @@ public class Shell {
 		while (h >= 1) {
 			for (int i = h; i < n; i++) {
 				for (int j = i; j >= h && a[j] < a[j - h]; j-=h) {
+					System.err.println("h:" + h + " " + Arrays.toString(a));
 					Exchanger.exchange(a, j, j - h);
+				}
+			}
+			h /= 3;
+		}
+	}
+
+	static void shell(int[] a) {
+		int n = a.length;
+		int h = 1;
+		while (h <= n / 3) {
+			h = h * 3 + 1;
+		}
+
+		while (h >= 1) {
+			for (int i = h; i < n; i++) {
+				for (int j = i; j >= h && a[j] < a[j - h]; j-=h) {
+					Exchanger.exchange(a, j, j - 1);
 				}
 			}
 			h /= 3;
