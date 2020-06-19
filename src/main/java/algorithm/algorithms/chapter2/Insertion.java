cb4403480c988ccc751f6acbf745071b49ac9ca3
unknown7 <unknown7@live.cn>
2020-06-19 17:24:55 +0800

    insertion

--- src/main/java/algorithm/algorithms/chapter2/Insertion.java
+++ src/main/java/algorithm/algorithms/chapter2/Insertion.java
@@ -12,10 +12,11 @@ public class Insertion {
 
 	static void sort(int[] a) {
 		for (int i = 1; i < a.length; i++) {
-			for (int j = i; j > 0; j--) {
-				if (a[j] < a[j - 1]) {
-					Exchanger.exchange(a, j, j - 1);
-				}
+			/**
+			 * 中断循环，值得思考，nice
+			 */
+			for (int j = i; j > 0 && a[j] < a[j - 1]; j--) {
+				Exchanger.exchange(a, j, j - 1);
 			}
 		}
 	}
